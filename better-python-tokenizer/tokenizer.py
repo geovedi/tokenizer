e94babbcf5b22d96bab2a025af87fe64f24681d4
Jim Geovedi <jim@geovedi.com>
2017-05-23 22:26:06 +0700

    faster tokenizer

--- better-python-tokenizer/tokenizer.py
+++ better-python-tokenizer/tokenizer.py
@@ -1,32 +1,59 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+
+import sys
 import string
-import regex as re
+import regex
 
 # TODO:
 # - language specific handling abbreviations and contractions
 # - detokenizer
 
 PUNCTS = set(string.punctuation)
-PUNCTS_PATTERN = re.compile(r'([^\p{P}]+|\p{S}|\p{P})')
-EMAIL = re.compile(r'([a-z\d\.-]+@[a-z\d\.-]+)', re.I)
-TWEET = re.compile(r'([@#][a-z\d]+)', re.I)
-URL = re.compile(r'([a-z\d]+:\/\/\S+)', re.I)
-DOMAIN = re.compile(r'(([a-z\d]+|[a-z\d][-])+[a-z\d]+){1,63}\.'
-                    '([a-z]{2,3}\.[a-z]{2}|[a-z]{2,6})', re.I)
-
+PUNCTS_PATTERN = regex.compile(r'([^\p{P}]+|[\p{S}\p{P}])', flags=regex.I)
+EMAIL = regex.compile(r'([a-z\d\.-]+@[a-z\d\.-]+)', flags=regex.I)
+URL = regex.compile(r'([a-z\d]+:\/\/\S+)', flags=regex.I)
+DOMAIN = regex.compile(r'^[a-z0-9]([a-z0-9-]+\.){1,}[a-z0-9]+\Z')
 
 def extract_entities(text):
     ents = set()
-    for token in re.split(r'\s+', text):
-        for pattern in [EMAIL, TWEET, URL, DOMAIN]:
-            match = pattern.search(token)
+    for token in regex.split(r'\s+', text):
+        if not token:
+            continue
+        if token[0] in '@#':
+            # twitter user and hashtag
+            ents.add(token)
+        elif '@' in token:
+            # could be email?
+            match = EMAIL.search(token)
             if match:
                 ents.add(match.group())
+        elif '://' in token:
+            # url
+            match = URL.search(token)
+            if match:
+                ents.add(match.group())
+        elif len(token.split('.')) >= 2:
+            match = DOMAIN.search(token)
+            if match:
+                ents.add(match.group())
+
     entities = {}
     for i, e in enumerate(sorted(ents, key=len, reverse=True)):
         eid = 'ENT_{0}'.format(i)
         entities[eid] = e
         text = text.replace(e, eid)
+
     return (text, entities)
+
+
+CONTRACTIONS = regex.compile(r"('(s|m|d|ll|re|ve)\W)|(n't\W)", flags=regex.I)
+
+def handle_contractions(text, sep='￭'):
+    text = CONTRACTIONS.sub(r' {sep}\1'.format(sep=sep), text)
+    return text
 
 
 EMPTY = 0
@@ -34,11 +61,9 @@ SPACE = 1
 PUNCT = 2
 OTHER = 3
 
-ADD_PREV = ((OTHER, EMPTY), (OTHER, SPACE), (OTHER, PUNCT), (PUNCT, PUNCT),
-            (PUNCT, EMPTY), (PUNCT, SPACE))
-
-ADD_NEXT = ((EMPTY, OTHER), (SPACE, OTHER), (PUNCT, OTHER), (EMPTY, PUNCT))
-
+ADD_PREV = ((OTHER, EMPTY), (OTHER, SPACE), (OTHER, PUNCT), (PUNCT, EMPTY), (PUNCT, SPACE))
+ADD_NEXT = ((EMPTY, OTHER), (SPACE, OTHER), (EMPTY, PUNCT), (PUNCT, PUNCT))
+ADD_BOTH = ((OTHER, OTHER), (PUNCT, OTHER), (PUNCT, OTHER))
 
 def check(text, mode=None):
     char = None
@@ -55,13 +80,15 @@ def check(text, mode=None):
         return PUNCT
     return OTHER
 
+MWE_PUNCTS = ',-/.:\''
 
-def tokenize(text, aggressive=False, separator='￭'):
+def tokenizer(text, aggressive=False, separator='￭'):
     psep = ' {0}'.format(separator)
     nsep = '{0} '.format(separator)
 
     # pre-processing
     text, entities = extract_entities(text)
+    text = handle_contractions(text)
 
     tokens = []
     parts = [''] + list(filter(None, PUNCTS_PATTERN.split(text))) + ['']
@@ -69,16 +96,21 @@ def tokenize(text, aggressive=False, separator='￭'):
     for pre, cur, nex in zip(parts, parts[1:], parts[2:]):
         if cur in PUNCTS:
             _aggressive = aggressive
-            if not cur in ',-/.:' and aggressive is True:
+            if not cur in MWE_PUNCTS and aggressive is True:
                 _aggressive = False
 
             (p, n) = (check(pre, 'pre'), check(nex, 'nex'))
+
+            # abbreviations?
+            if cur == '.' and (len(pre.strip()) == 1 or len(nex.strip()) == 1) and (p == OTHER or n == OTHER):
+                tokens.append('{cur}'.format(cur=cur))
+                continue
 
             if (p, n) in ADD_NEXT:
                 tokens.append('{cur}{nsep}'.format(cur=cur, nsep=nsep))
             elif (p, n) in ADD_PREV:
                 tokens.append('{psep}{cur}'.format(cur=cur, psep=psep))
-            elif (p, n) == (OTHER, OTHER) and _aggressive:
+            elif (p, n) in ADD_BOTH and _aggressive:
                 tokens.append(
                     '{psep}{cur}{nsep}'.format(cur=cur, psep=psep, nsep=nsep))
             else:
@@ -92,3 +124,9 @@ def tokenize(text, aggressive=False, separator='￭'):
         text = text.replace(eid, ent)
 
     return text.split()
+
+
+if __name__ == '__main__':
+    for line in sys.stdin:
+        print(' '.join(tokenizer(line.strip(), aggressive=True)))
+
